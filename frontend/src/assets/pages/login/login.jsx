cca3b728f9571b2fafeee8f0adc3f39a77229ad0
JonathanAslam <jonathanaslam@csu.fullerton.edu>
2025-10-02 12:58:11 -0700

    staging commit before pull incase changes are lost

--- frontend/src/assets/pages/login/login.jsx
+++ frontend/src/assets/pages/login/login.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import './Login.css'
+import Button from '../../components/button/Button'
 
 
 const login = () => {
+
+  const [hasAccount, setHasAccount] = useState(false);
+
+  // useEffect(() => {
+
+
+  // }, []);
+
+
   return (
-    <div>
-      <form action="">
-        <h1>Login</h1>
-      </form>
-    </div>
+    <div className='login-page'>
+      <div className='forms-container'>
+        {hasAccount ? (
+          //Login form
+          < div className='form'>
+            <h1>Login</h1>
+            <form action="" className='input-field'>
+              <input type="text" placeholder='Username' />
+              <input type="email" placeholder='Email' />
+              <input type="password" placeholder='Password' />
+              <button className='btn'>Login</button>
+            </form>
+          </div>
+        ) : (
+          //Create account form
+          < div className='form' >
+            <h1>Create Account</h1>
+            <form action="" className='input-field'>
+              <input type="text" placeholder='Username' />
+              <input type="email" placeholder='Email' />
+              <input type="password" placeholder='Password' />
+              <button className='btn'>Create Account</button>
+            </form>
+          </div >
+        )}
+        <Button
+          text={hasAccount ? "Don't Have An Account? Create One Here!" : "Have An Account. Login Here!"} onClick={() => setHasAccount(!hasAccount)}
+        />
+      </div>
+    </div >
   )
 }
 
