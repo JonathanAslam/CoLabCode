29b857b2244778402cc4f78ff543fbf8e67fb119
JonathanAslam <jonathanaslam@csu.fullerton.edu>
2025-10-02 14:19:18 -0700

    updated login page. still working on it

--- frontend/src/assets/pages/login/Login.jsx
+++ frontend/src/assets/pages/login/Login.jsx
@@ -4,29 +4,39 @@ import Button from '../../components/button/Button'
 
 
 const Login = () => {
+  const [hasAccount, setHasAccount] = useState(false)
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, email, password);
+  }
+
   return (
     <div className='login-page'>
       <div className='forms-container'>
         {hasAccount ? (
           //Login form
-          < div className='form'>
+          < div className='form' onSubmit={handleSubmit}>
             <h1>Login</h1>
             <form action="" className='input-field'>
-              <input type="text" placeholder='Username' />
-              <input type="email" placeholder='Email' />
-              <input type="password" placeholder='Password' />
-              <button className='btn'>Login</button>
+              <input type="text" placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
+              <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+              <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+              <button className='btn' type="submit">Login</button>
             </form>
           </div>
         ) : (
           //Create account form
-          < div className='form' >
+          < div className='form' onSubmit={handleSubmit}>
             <h1>Create Account</h1>
             <form action="" className='input-field'>
-              <input type="text" placeholder='Username' />
-              <input type="email" placeholder='Email' />
-              <input type="password" placeholder='Password' />
-              <button className='btn'>Create Account</button>
+              <input type="text" placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
+              <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+              <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+              <button className='btn' type="submit">Create Account</button>
             </form>
           </div >
         )}
