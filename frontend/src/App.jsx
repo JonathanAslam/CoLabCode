09b0dadc0da930dc626080280a7cb7380fadab09
JonathanAslam <jonathanaslam@csu.fullerton.edu>
2025-11-01 21:05:02 -0700

    updated filepath for Login.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './assets/components/navbar/Navbar'
 import Homepage from './assets/pages/homepage/HomePage'
 import Dashboard from './assets/pages/dashboard/Dashboard'
 import About from './assets/pages/about/About'
-import Login from './assets/pages/login/login'
+import Login from './assets/pages/login/Login'
 import Document from './assets/pages/document/Document'
 
 
